5c9db5218fe34d5798a10665e33ef2b026bc13ae
MatanYZ1818 <matniredon10@Gmail.com>
2023-07-17 16:49:36 +0300

    added memory game

---
+++ games/memory/memory.js
@@ -0,0 +1,19 @@
+const difficulty=[{num:1,text:"easy"},{num:2,text:"medium"},{num:3,text:"hard"}]
+
+let cardsArr=[]
+
+function showDiff(){
+    console.log("show diff")
+    const buttons=document.getElementById("lowerButtons")
+    buttons.style.top="-160px"
+    buttons.style.visibility="visible"
+}
+
+function showCards(cards){
+    const shade=document.getElementById("game_shade")
+    shade.style
+}
+
+function createCards(dif){
+
+}
